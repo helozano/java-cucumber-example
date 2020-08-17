ac71be4c83b42369be948f4b0eb3813be2044fd7
helozano <helozano.sistemas@gmail.com>
2020-08-17 13:33:53 -0500

    Update SimpleTest.java

--- src/test/java/selenium/SimpleTest.java
+++ src/test/java/selenium/SimpleTest.java
@@ -3,21 +3,21 @@ package selenium;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriver.Options;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 import support.Browser;
 
 public class SimpleTest {
 
   @Test
   public void sometest() {
-    chrome_options = Options();
-    chrome_options.add_argument("--headless");
-    chrome_options.add_argument("--no-sandbox");
-    chrome_options.add_argument("--disable-dev-shm-usage");
+    ChromeDriverManager.getInstance().setup();
+    ChromeOptions chromeOptions = new ChromeOptions();
+    chromeOptions.add_argument("--no-sandbox");
+    chromeOptions.addArguments("--headless");
     System.setProperty("webdriver.chrome.driver", System.getProperty("driverpath"));
-    WebDriver driver = new ChromeDriver(chrome_options=chrome_options);
+    WebDriver driver = new ChromeDriver(chrome_options=chromeOptions);
     driver.get("https://www.google.com");
     WebElement element = driver.findElement(By.name("q"));
     element.sendKeys("Cheese!");
