481475e9d66e39bfd373c399b70fa71e5497ccb7
helozano <helozano.sistemas@gmail.com>
2020-08-17 13:53:28 -0500

    Update SimpleTest.java

--- src/test/java/selenium/SimpleTest.java
+++ src/test/java/selenium/SimpleTest.java
@@ -1,15 +1,27 @@
 package selenium;
 
+ 
+
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+ 
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-import support.Browser;
+ 
+
+//import support.Browser;
+
+ 
 
 public class SimpleTest {
+
+ 
 
   @Test
   public void sometest() {
@@ -20,7 +32,8 @@ public class SimpleTest {
     options.addArguments("--headless"); 
     options.addArguments("--disable-gpu"); 
     System.setProperty("webdriver.chrome.driver", System.getProperty("driverpath"));
-    WebDriver driver = Browser.launch(options);
+   // WebDriver driver = Browser.launch(options);
+    WebDriver driver = new ChromeDriver(options);
     driver.get("https://www.google.com");
     WebElement element = driver.findElement(By.name("q"));
     element.sendKeys("Cheese!");
