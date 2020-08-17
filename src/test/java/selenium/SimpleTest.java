deb0d531315473de2c31353bc0934aa60c0e631f
helozano <helozano.sistemas@gmail.com>
2020-08-17 13:04:33 -0500

    Update SimpleTest.java

--- src/test/java/selenium/SimpleTest.java
+++ src/test/java/selenium/SimpleTest.java
@@ -14,8 +14,13 @@ public class SimpleTest {
   public void sometest() {
     //http://www.seleniumhq.org/download/maven.jsp
     //https://code.google.com/p/selenium/wiki/GettingStarted
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     System.setProperty("webdriver.chrome.driver", System.getProperty("driverpath"));
-    WebDriver driver = new ChromeDriver();
+    System.setProperty("webdriver.chrome.driver", System.getProperty("driverpath"));
+    WebDriver driver = new ChromeDriver(chrome_options=chrome_options);
     driver.get("https://www.google.com");
     WebElement element = driver.findElement(By.name("q"));
     element.sendKeys("Cheese!");
