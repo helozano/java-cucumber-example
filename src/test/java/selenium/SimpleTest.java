49a98dbb0b83dcf4fdcb54f5d6997821f781d74f
helozano <helozano.sistemas@gmail.com>
2020-08-17 13:07:23 -0500

    Update SimpleTest.java

--- src/test/java/selenium/SimpleTest.java
+++ src/test/java/selenium/SimpleTest.java
@@ -14,11 +14,10 @@ public class SimpleTest {
   public void sometest() {
     //http://www.seleniumhq.org/download/maven.jsp
     //https://code.google.com/p/selenium/wiki/GettingStarted
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    System.setProperty("webdriver.chrome.driver", System.getProperty("driverpath"));
+    chrome_options = Options();
+    chrome_options.add_argument('--headless');
+    chrome_options.add_argument('--no-sandbox');
+    chrome_options.add_argument('--disable-dev-shm-usage');
     System.setProperty("webdriver.chrome.driver", System.getProperty("driverpath"));
     WebDriver driver = new ChromeDriver(chrome_options=chrome_options);
     driver.get("https://www.google.com");
