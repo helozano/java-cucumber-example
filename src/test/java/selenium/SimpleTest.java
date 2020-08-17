b29aaba9c02c96c4b57062b26c320272a3a7c691
helozano <helozano.sistemas@gmail.com>
2020-08-17 12:43:07 -0500

    Update SimpleTest.java

--- src/test/java/selenium/SimpleTest.java
+++ src/test/java/selenium/SimpleTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.WebDriverManager;
 
 import support.Browser;
 
