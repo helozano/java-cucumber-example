1fe9fbfe238922c03ce6e3f8cd9c299f9b614864
helozano <helozano.sistemas@gmail.com>
2020-08-24 17:44:35 -0500

    Update SimpleTest.java

--- src/test/java/selenium/SimpleTest.java
+++ src/test/java/selenium/SimpleTest.java
@@ -8,35 +8,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
- 
+String browser = "chrome";
+public static String hubAddress = "http://localhost:4444/wd/hub
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
- 
-
-import support.Browser;
-
- 
-
-public class SimpleTest {
-
- 
-
+ public class SimpleTest {
   @Test
   public void sometest() {
     //http://www.seleniumhq.org/download/maven.jsp
     //https://code.google.com/p/selenium/wiki/GettingStarted
-    WebDriverManager.chromedriver().setup();
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--no-sandbox");
-    options.addArguments("--headless"); 
-    options.addArguments("--disable-gpu");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--remote-debugging-port=9222");
-    System.setProperty("webdriver.chrome.driver", System.getProperty("driverpath"));
-   // WebDriver driver = Browser.launch(options);
-    WebDriver driver = new ChromeDriver(options);
+    // WebDriver driver = Browser.launch(options);
+    DesiredCapabilities caps = new DesiredCapabilities();
+    caps.setCapability(ChromeOptions.CAPABILITY,  chromeOpts);
+    caps.setCapability("browserName", "googlechrome");
+    caps.setCapability("browserVersion", "latest");
+    String sauceUrl = "http://localhost:4444/wd/hub";
+    URL url = new URL(sauceUrl);
+    driver = new RemoteWebDriver(url, caps);
     driver.get("https://www.google.com");
     WebElement element = driver.findElement(By.name("q"));
     element.sendKeys("Cheese!");
